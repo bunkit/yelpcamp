91da5bd93c92bdb624ea5bc1e06f7e587927bef2
Bangkit <>
2019-12-31 15:54:32 +0700

    day 2

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-
-
-app.set('view engine', 'ejs');
-
-app.get('/', function (req,res) { 
-    res.render('landing');
-});
-
-app.get('/campgrounds', function (req,res) { 
-    let campgrounds = [
+ let campgrounds = [
         {
             name: "Thelma Fort",
             image: "http://lorempixel.com/500/250/city/1"
@@ -52,15 +44,33 @@ app.get('/campgrounds', function (req,res) {
             image: "http://lorempixel.com/500/250/city/10"
         }
     ];
+//for post rncode
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req,res) { 
+    res.render('landing');
+});
+
+app.get('/campgrounds', function (req,res) { 
     res.render('campgrounds', {campgrounds:campgrounds});
 });
 
 app.post('/campgrounds', function(req,res) {
-    res.send('post hitted');
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {name: name, image:image}
+    campgrounds.push(newCampground);
+
+    res.redirect('/campgrounds');
     // get data from form and add to camgrounds array
     // redirect back to campgrounds page
-
 });
+
+app.get('/campgrounds/new', function (req,res) {
+    res.render('new');
+})
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
     console.log('The YelpCamp Server has started');
