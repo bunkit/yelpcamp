a88dff925e6bfbea854a13fac99d0e4aaa6850a3
Bangkit <>
2020-01-15 15:12:40 +0700

    seeding data comment

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect('mongodb://localhost/yelp_camp', {
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-// RESETING DATA
+// RECREATING DATA
 seedDB();
 
 // HOMEPAGE
