8ef98c38f77e969875488d1eb4aa55817bf8243a
Bangkit <>
2020-01-20 13:31:36 +0700

    styling

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://localhost/yelp_camp', {
 //FOR POST RNCODE
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
 
 // RECREATING DATA
 seedDB();
